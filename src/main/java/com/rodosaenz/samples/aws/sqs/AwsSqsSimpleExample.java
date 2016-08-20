65902b37389ce41c653ea15b45673258416aa897
Rodolfo Saenz <rodolfo.saenz@quanticvision.com>
2016-08-19 23:50:26 -0500

    First Commit

--- src/main/java/com/rodosaenz/samples/aws/sqs/AwsSqsSimpleExample.java
+++ src/main/java/com/rodosaenz/samples/aws/sqs/AwsSqsSimpleExample.java
@@ -12,7 +12,7 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-package com.rodosaenz.examples.aws.sqs;
+package com.rodosaenz.samples.aws.sqs;
 
 
 import com.amazonaws.AmazonClientException;
